6a5cbd65ce19a8ac8fa31addcd3a14bed0347e44
Italo Barros <italobarroscontato@gmail.com>
2022-04-26 23:01:45 -0300

    Creating WelcomeMessage Function

--- backend/server/pipelines/pipeline.go
+++ backend/server/pipelines/pipeline.go
@@ -6,8 +6,27 @@ import (
 )
 
 func ChainProcess(Message *models.Message) error {
-	Message.ProcessedAt = true
+	err := WelcomeMessage(Message)
+	return err
+}
+
+func WelcomeMessage(Message *models.Message) error {
+	var AnswerMessage models.Message
 	db := database.GetDatabase()
+	if !Message.ProcessedAt && Message.Step == 1 && Message.WidSender != "Numero do italo" {
+		Message.ProcessedAt = true
+		AnswerMessage.WidSender = "Numero do Ítalo"
+		AnswerMessage.WidReceiver = Message.WidSender
+		AnswerMessage.Message = "Olá, Boa noite! \n \n Conhece nossas opções?"
+		AnswerMessage.ProcessedAt = true
+		AnswerMessage.Step = 999
+		err := db.Save(&AnswerMessage).Error
+		if err != nil {
+			return err
+		}
+		Message.Step = 2
+	}
+	Message.ProcessedAt = true
 	err := db.Save(&Message).Error
 	return err
 }
