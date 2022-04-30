04973efb44c798240aaaf98be4beca52c46bca5e
Abraao Lacerda <abraaobarros3@gmail.com>
2022-04-30 20:46:08 -0300

    Removing whatsapp folder

--- server/database/database.go
+++ server/database/database.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"app/server/database/migrations"
-	"fmt"
 	"log"
 	"os"
 	"time"
@@ -14,16 +13,18 @@ import (
 var db *gorm.DB
 
 func StartDB() {
-	port := os.Getenv("DATABASE_PORT")
-	url := os.Getenv("DATABASE_HOST")
-	user := os.Getenv("DATABASE_USER")
-	password := os.Getenv("DATABASE_PASSWORD")
-	dbname := os.Getenv("DATABASE_NAME")
 
-	str := "host=" + url + " port=" + port + " user=" + user + " dbname=" + dbname + " sslmode=disable password=" + password
-
-	fmt.Println(str)
-	database, err := gorm.Open(postgres.Open(str), &gorm.Config{})
+	uri := os.Getenv("DATABASE_URL")
+	database, err := gorm.Open(postgres.Open(uri), &gorm.Config{})
+	if err != nil {
+		port := os.Getenv("DATABASE_PORT")
+		url := os.Getenv("DATABASE_HOST")
+		user := os.Getenv("DATABASE_USER")
+		password := os.Getenv("DATABASE_PASSWORD")
+		dbname := os.Getenv("DATABASE_NAME")
+		str := "host=" + url + " port=" + port + " user=" + user + " dbname=" + dbname + " sslmode=disable password=" + password
+		database, err = gorm.Open(postgres.Open(str), &gorm.Config{})
+	}
 
 	if err != nil {
 		log.Fatal("error: ", err)
