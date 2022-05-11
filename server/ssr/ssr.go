7272e80d1ba901fcfec77ebf3cf08149abc4e5f8
Abraao Lacerda <abraaobarros3@gmail.com>
2022-05-11 13:05:54 -0300

    Move ssr to server

---
+++ server/ssr/ssr.go
@@ -0,0 +1,32 @@
+package ssr
+
+import (
+	"app/server/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func index(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "index.html", gin.H{
+		"title": "Title",
+	})
+}
+
+func messages(ctx *gin.Context) {
+	m := []models.Message{
+		{Message: "Teste"},
+		{Message: "Teste2"},
+	}
+	ctx.HTML(http.StatusOK, "list.html", gin.H{
+		"messages": m,
+	})
+}
+
+func ServerSideHandler(router *gin.Engine) *gin.Engine {
+	router.LoadHTMLGlob("./server/ssr/tmpl/**/*.html")
+	router.GET("/messages", messages)
+	router.GET("/", index)
+	return router
+
+}
