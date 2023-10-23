e9292ea384ea941cfb4bcaea44f5b67f2a1d2549
Italo <italobarroscontato@gmail.com>
2023-10-22 22:54:28 -0300

    GetRoleMessagesFromFaq

--- server/pipelines/pipeline.go
+++ server/pipelines/pipeline.go
@@ -8,13 +8,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
-func TemplateResponse(b bots.Bot, c models.Costumer, Message *models.Message) error {
+func TemplateResponse(b bots.Bot, c models.Customer, Message *models.Message) error {
 	user := getUserFromMessage(c, *Message)
 	state := b.GetStateTemplate(c.Wid, user)
 	if state == "end" {
@@ -26,20 +28,112 @@ func TemplateResponse(b bots.Bot, c models.Costumer, Message *models.Message) er
 	return err
 }
 
-func GetRoleMessages(b bots.Bot, c models.Costumer, userNumber string) []RoleMessage {
+func GetRoleMessagesFromFaq(c models.Customer, message models.Message) []RoleMessage {
+	dbSql := database.GetDatabaseSql()
+	var roleMessages []RoleMessage
+	gptPayload := GPTEbdPayload{
+		Model: "text-embedding-ada-002",
+		Input: message.Message,
+	}
+
+	fmt.Printf("%+v", gptPayload)
+	gptPayloadBody, err := json.Marshal(gptPayload)
+	if err != nil {
+
+		return []RoleMessage{}
+	}
+
+	gptPayloadStream := bytes.NewBuffer(gptPayloadBody)
+	client := http.Client{}
+	req, err := http.NewRequest("POST", "https://api.openai.com/v1/embeddings", gptPayloadStream)
+	if err != nil {
+
+		return []RoleMessage{}
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	token := os.Getenv("OPENIA_TOKEN")
+	req.Header.Add("Authorization", "Bearer "+token)
+	res, err := client.Do(req)
+
+	if err != nil {
+
+		return []RoleMessage{}
+
+	}
+	defer res.Body.Close()
+	bytes, err := io.ReadAll(res.Body)
+	if err != nil {
+
+		return []RoleMessage{}
+	}
+	var aiResponse EbdResponse
+	err = json.Unmarshal([]byte(bytes), &aiResponse)
+	if err != nil {
+
+		return []RoleMessage{}
+	}
+	// for _, message := range aiResponse.Data {
+	// 	fmt.Println("xxVETORxx")
+	// 	fmt.Println(message.Vector)
+	// 	fmt.Println("xxxxxx")
+	// }
+
+	vector := convertToPostgresArray(aiResponse.Data[0].Vector)
+	var faqs []models.Faq
+	fmt.Println("Iniciando busca de faqs baseado na proximidade do vetor...")
+	rows, err := dbSql.Query("SELECT * FROM faqs WHERE customer_id = $1 ORDER BY embedding <-> $2 LIMIT 2", c.ID, vector)
+	if err != nil {
+		log.Fatalf("Erro ao executar a consulta: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var faq models.Faq
+		err = rows.Scan(&faq.ID, &faq.Question, &faq.Answer, &faq.Embedding, &faq.CustomerId)
+		fmt.Println("FAQ: " + faq.Question)
+		if err != nil {
+
+			log.Fatalf("Erro ao escanear a consulta: %v", err)
+
+		}
+		faqs = append(faqs, faq)
+	}
+	//um loop para cada faq transformando em roleMessage com o atributo Role = "system" e Content = faq.Question+"; "+faq.Answer
+	for _, faq := range faqs {
+		roleMessages = append(roleMessages, RoleMessage{
+			Role:    "system",
+			Content: faq.Question + "; " + faq.Answer,
+		})
+	}
+	return roleMessages
+}
+
+func GetRoleMessagesFromCostumer(c models.Customer, message models.Message) []RoleMessage {
+	db := database.GetDatabase()
+	var roleMessages []RoleMessage
+	var customerRoleMessages []models.CustomerRoleMessage
+	fmt.Println("Iniciando busca de RoleMessages baseado no Customer...")
+	db.Where("customer_id = ?", c.ID).Find(&customerRoleMessages)
+	//um loop para cada customerRoleMessage transformando em roleMessage e adicionando na lista roleMessages com o atributo Role = customerRoleMessage.Role e Content = customerRoleMessage.Message
+	for _, customerRoleMessage := range customerRoleMessages {
+		roleMessages = append(roleMessages, RoleMessage{
+			Role:    customerRoleMessage.Role,
+			Content: customerRoleMessage.Message,
+		})
+	}
+	return roleMessages
+}
+
+func GetRoleMessages(b bots.Bot, c models.Customer, userNumber string, message models.Message) []RoleMessage {
 	db := database.GetDatabase()
 	var messages []models.Message
 	var roleMessages []RoleMessage
 	session := b.GetSession(c.Wid, userNumber)
-	fmt.Println("Adicionando contexto de sistema...")
-	roleMessages = append(roleMessages, RoleMessage{
-		Role:    "system",
-		Content: "Você se chama Delillah, uma assistente virtual GPT, muito educada, organizada, e proativa.",
-	})
-	roleMessages = append(roleMessages, RoleMessage{
-		Role:    "system",
-		Content: "A primeira interação com o usuário, voce se apresenta",
-	})
+	//Adicionar a lista roleMessages o retorno da função GetRoleMessagesFromFaq
+	roleMessages = append(roleMessages, GetRoleMessagesFromFaq(c, message)...)
+	roleMessages = append(roleMessages, GetRoleMessagesFromCostumer(c, message)...)
+
 	fmt.Println("Iniciando busca de mensagens da sessão...")
 	db.Where("session_id = ?", session.ID).Find(&messages)
 	for _, message := range messages {
@@ -62,12 +156,22 @@ func GetRoleMessages(b bots.Bot, c models.Costumer, userNumber string) []RoleMes
 	return roleMessages
 }
 
-func GetGPTResponse(b bots.Bot, c models.Costumer, Message *models.Message) (string, error) {
+func convertToPostgresArray(vector []float64) string {
+	// Converter o vetor Go em uma string
+	vectorString := fmt.Sprintf("%v", vector)
+
+	// Remover os espaços da string e adicionar as aspas
+	vectorString = "" + strings.Replace(vectorString, " ", ",", -1) + ""
+
+	return vectorString
+}
+
+func GetGPTResponse(b bots.Bot, c models.Customer, Message *models.Message) (string, error) {
 	user := getUserFromMessage(c, *Message)
-	gptMessages := GetRoleMessages(b, c, user)
+	gptMessages := GetRoleMessages(b, c, user, *Message)
 
 	gptPayload := GPTPayload{
-		Model:            "gpt-3.5-turbo",
+		Model:            "gpt-4-0613",
 		Messages:         gptMessages,
 		MaxTokens:        1500,
 		Temperature:      0.33,
@@ -130,7 +234,7 @@ func GetGPTResponse(b bots.Bot, c models.Costumer, Message *models.Message) (str
 	return finalResponse, nil
 }
 
-func ChangeStateBasedOnSelectedOption(b bots.Bot, c models.Costumer, Message *models.Message) error {
+func ChangeStateBasedOnSelectedOption(b bots.Bot, c models.Customer, Message *models.Message) error {
 	user := getUserFromMessage(c, *Message)
 	Option, err := strconv.Atoi(Message.Message)
 	if err != nil && (b.GetStateTemplate(c.Wid, user) != b.GetFirstTemplate(c.Wid)) {
@@ -155,7 +259,7 @@ func ChangeStateBasedOnSelectedOption(b bots.Bot, c models.Costumer, Message *mo
 	return err
 }
 
-func ChangeStateBasedStatus(b bots.Bot, c models.Costumer, Message *models.Message) (models.Session, error) {
+func ChangeStateBasedStatus(b bots.Bot, c models.Customer, Message *models.Message) (models.Session, error) {
 	user := getUserFromMessage(c, *Message)
 
 	// if b.GetSession(c.Wid, user).State == "INITIAL" {
@@ -166,32 +270,32 @@ func ChangeStateBasedStatus(b bots.Bot, c models.Costumer, Message *models.Messa
 	return session, nil
 }
 
-func getUserFromMessage(c models.Costumer, m models.Message) string {
+func getUserFromMessage(c models.Customer, m models.Message) string {
 	if c.Wid == m.WidSender {
 		return m.WidReceiver
 	}
 	return m.WidSender
 }
 
-func checkStateOptions(b bots.Bot, c models.Costumer, user string, options []int, Option int) bool {
+func checkStateOptions(b bots.Bot, c models.Customer, user string, options []int, Option int) bool {
 	if len(options) == 0 {
 		return (strconv.FormatUint(uint64(c.RateTemplateID), 10) == b.GetStateTemplate(c.Wid, user) && (Option < 1 || Option > 3))
 	}
 	return Option > len(options) || Option < 1
 }
 
-func ResetStateTemplate(b bots.Bot, c models.Costumer, Message *models.Message) error {
+func ResetStateTemplate(b bots.Bot, c models.Customer, Message *models.Message) error {
 	user := getUserFromMessage(c, *Message)
 	var Session models.Session
 	db := database.GetDatabase()
-	db.Where("wid_costumer = ? AND wid_user = ?", c.Wid, user).Last(&Session)
+	db.Where("wid_Customer = ? AND wid_user = ?", c.Wid, user).Last(&Session)
 	if getConditionsToReset(Message.Message, Session.CreatedAt) {
 		b.SetState(b.GetFirstTemplate(c.Wid), c.Wid, user)
 	}
 	return nil
 }
 
-func ResetState(b bots.Bot, c models.Costumer, Message *models.Message) (models.Session, error) {
+func ResetState(b bots.Bot, c models.Customer, Message *models.Message) (models.Session, error) {
 	user := getUserFromMessage(c, *Message)
 	session := b.GetSession(c.Wid, user)
 	if getConditionsToReset(Message.Message, session.UpdateAt) && session.State != "INITIAL" {
@@ -207,7 +311,7 @@ func getConditionsToReset(message string, updatedAt time.Time) bool {
 	return check
 }
 
-func ChainProcess(b bots.Bot, c models.Costumer, Message *models.Message) error {
+func ChainProcess(b bots.Bot, c models.Customer, Message *models.Message) error {
 	ResetStateTemplate(b, c, Message)
 	ChangeStateBasedOnSelectedOption(b, c, Message)
 	TemplateResponse(b, c, Message)
@@ -217,7 +321,7 @@ func ChainProcess(b bots.Bot, c models.Costumer, Message *models.Message) error 
 	return nil
 }
 
-func ChainProcessGPT(b bots.Bot, c models.Costumer, Message *models.Message) (string, error) {
+func ChainProcessGPT(b bots.Bot, c models.Customer, Message *models.Message) (string, error) {
 	session, err := ResetState(b, c, Message)
 	if session.State == "CLOSED" {
 		Message.SessionID = session.ID
@@ -278,4 +382,21 @@ type GPTPayload struct {
 	TopP             int           `json:"top_p"`
 	FrequencyPenalty int           `json:"frequency_penalty"`
 	PresencePenalty  int           `json:"presence_penalty"`
+}
+
+type GPTEbdPayload struct {
+	Model string `json:"model"`
+	Input string `json:"input"`
+}
+
+type EbdObject struct {
+	Vector []float64 `json:"embedding"`
+	Index  int       `json:"index"`
+	Object string    `json:"object"`
+}
+
+type EbdResponse struct {
+	Data   []EbdObject `json:"data"`
+	Model  string      `json:"model"`
+	Object string      `json:"object"`
 }
