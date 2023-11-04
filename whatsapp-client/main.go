1aff7264c90d500b051e107bda2f59c43c6cfd68
Italo <italobarroscontato@gmail.com>
2023-11-03 21:38:33 -0300

    change heroku config

--- whatsapp-client/main.go
+++ whatsapp-client/main.go
@@ -85,11 +85,11 @@ func getResponseText(message string) (string, error) {
 }
 
 func dbConn() string {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
+	host := os.Getenv("DATABASE_HOST")
+	port := os.Getenv("DATABASE_PORT")
+	user := os.Getenv("DATABASE_USER")
+	password := os.Getenv("DATABASE_PASSWORD")
+	dbname := os.Getenv("DATABASE_NAME")
 
 	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 }
